6ff734d67f7cd0f0a2d6c1937a21aaa57866a90b
PZ <wikiwoo@PZ.local>
2023-08-08 20:32:24 +0300

    with useMemo

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import EventCard from './EventCard';
 import Buttons from '../Buttons/Buttons'
 import { useSelector, useDispatch } from 'react-redux';
@@ -8,32 +8,21 @@ import styles from './events.module.scss';
 import goback from '../../images/goback.svg';
 
 const Events = () => {
-  const dispatch = useDispatch()
   const events = useSelector(store => store.events);
-  // console.log(events)
-  const [fil, setFil] = useState(useSelector(store => store.filter.search))
-  const [allEvents, setAllEvents] = useState(events.filter(item => (item.title.toLowerCase()).includes(fil.toLowerCase())));
-  console.log(allEvents)
-  
-  console.log(fil.length)
+  const fil = useSelector(store => store.filter.search);
+  const dispatch = useDispatch()
 
-  const handleClear = (e) => {
-    e.preventDefault();
-    setFil('')
-    dispatch(setFilter(fil))
-    setAllEvents(events)
-  }
- 
-  // useEffect(() => {
-  //   setAllEvents(events.filter(item => (item.title.toLowerCase()).includes(fil.toLowerCase())))
-  // }, [fil])
+  const allEvs = useMemo(() => {
+    return events.filter(item => (item.title.toLowerCase()).includes(fil.toLowerCase()))
+  }, [fil, events])
+
 
   return (
     <div >
        <Buttons/>
-       <button className='goback' onClick={handleClear}><img src={goback}/></button>
-         <div className={styles.events}>   
-      {allEvents?.length ? allEvents.map(event => (
+          <button className='goback' onClick={() => dispatch(setFilter(''))}>Back to all events</button>
+          <div className={styles.events}>   
+            {allEvs?.length ? allEvs.map(event => (
         <EventCard event={event} key={event.id}/>
       )) : (<h3>No events</h3>)}
     </div>
