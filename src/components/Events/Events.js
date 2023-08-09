092b0d06d8d06bb97589c3ed9128f23de3a820c2
PeterZabila <petrozabila@gmail.com>
2023-08-09 09:40:23 +0300

    added category filter

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -2,28 +2,41 @@ import React, { useEffect, useState, useMemo } from 'react';
 import EventCard from './EventCard';
 import Buttons from '../Buttons/Buttons'
 import { useSelector, useDispatch } from 'react-redux';
-import { setFilter } from '../../reducers/filter';
+import { setFilter, setCategory } from '../../reducers/filter';
 // import { setFilter, setSort, setCategory } from '../../reducers/filter';
 import styles from './events.module.scss';
-import goback from '../../images/goback.svg';
 
 const Events = () => {
   const events = useSelector(store => store.events);
   const fil = useSelector(store => store.filter.search);
+  const sort = useSelector(store => store.filter.sort);
+  const category = useSelector(store => store.filter.category);
   const dispatch = useDispatch()
 
-  const allEvs = useMemo(() => {
-    return events.filter(item => (item.title.toLowerCase()).includes(fil.toLowerCase()))
-  }, [fil, events])
+  const result = useMemo(() => {
+    if (fil) {
+     return events.filter(item => (item.title.toLowerCase()).includes(fil.toLowerCase()));
+    } if (category) {
+      return events.filter(item => (item.category.toLowerCase() === category.toLowerCase()));
+    }
+    else {
+      return events;
+    }
 
+  }, [fil, events, category])
+
+  const resetFilters = () => {
+    dispatch(setFilter(''));
+    dispatch(setCategory(''))
+  }
 
   return (
     <div >
        <Buttons/>
-       {fil ? (<button className='goback' onClick={() => dispatch(setFilter(''))}>Back to all events</button>) : null}
+       {fil || category ? (<button className='goback' onClick={resetFilters}>Back to all events</button>) : null}
           
           <div className={styles.events}>   
-            {allEvs?.length ? allEvs.map(event => (
+            {result?.length ? result.map(event => (
         <EventCard event={event} key={event.id}/>
       )) : (<h3>No events</h3>)}
     </div>
